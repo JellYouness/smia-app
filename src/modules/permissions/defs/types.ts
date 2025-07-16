667364b1882548eb7bff97ac20462244da835b27
Oussama Chaoui <oussamaqqqq@gmail.com>
2025-07-16 14:46:37 +0100

    Refactor roles and enhance index page loading state
    - Updated ROLE enum to include new roles: CLIENT, CREATOR, AMBASSADOR, and SYSTEM_ADMINISTRATOR.
    - Enhanced the index page to display a loading state while user authentication is processed.
    - Added conditional rendering for the PageHeader based on user roles, specifically for CLIENT role.
    - Improved server-side translations to include additional namespaces for better localization.

--- src/modules/permissions/defs/types.ts
+++ src/modules/permissions/defs/types.ts
@@ -1,9 +1,10 @@
 import { CRUD_ACTION, Id } from '@common/defs/types';
 
 export enum ROLE {
-  ADMIN = 'admin',
-  USER = 'user',
-  TEST = 'test',
+  CLIENT = 'client',
+  CREATOR = 'creator',
+  AMBASSADOR = 'ambassador',
+  SYSTEM_ADMINISTRATOR = 'system_administrator',
 }
 
 export interface Permission {
